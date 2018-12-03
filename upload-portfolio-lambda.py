06254f46d5b78a789c7b156e7cde66a0b6073f6c
pranav16.dev <pranav16.dev@gmail.com>
2018-12-02 22:42:35 -0500

    upload build script

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,18 @@
+import boto3
+import io
+import zipfile
+import mimetypes
+
+s3 = boto3.resource('s3')
+portfolio_bkt = s3.Bucket('portfolio.patelsalescorp.com')
+
+build_bkt = s3.Bucket('portfoliobuildwest.patelsalescorp.com')
+
+portfolio_zip = io.BytesIO()
+build_bkt.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        portfolio_bkt.upload_fileobj(obj,nm,ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+        portfolio_bkt.Object(nm).Acl().put(ACL='public-read')
