98bf4e4e13542ff107b1b0db88514215a05621f7
pranav16.dev <pranav16.dev@gmail.com>
2018-12-03 00:06:21 -0500

    adding code pipleine to the portfolio

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -10,14 +10,26 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic  = sns.Topic('arn:aws:sns:us-west-2:182934946837:deployPortfolioTopic')
     
+    location = {
+        "bucketName": 'portfoliobuildwest.patelsalescorp.com',
+        "objectKey": 'portfoliobuild.zip'
+    }
     try:
+        job = event.get("CodePipeline.job")
+        
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+                        
+        print("Building PortFolio from")
         s3 = boto3.resource('s3')
         portfolio_bkt = s3.Bucket('portfolio.patelsalescorp.com')
         
-        build_bkt = s3.Bucket('portfoliobuildwest.patelsalescorp.com')
+        build_bkt = s3.Bucket(location["bucketName"])
         
         portfolio_zip = io.BytesIO()
-        build_bkt.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        build_bkt.download_fileobj(location["objectKey"], portfolio_zip)
         
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -26,6 +38,10 @@ def lambda_handler(event, context):
                 portfolio_bkt.Object(nm).Acl().put(ACL='public-read')
        
         topic.publish(Subject='Portfolio for Patel Sales Corporation uploaded', Message='Please check your PortFolio Deployed Successfully')
+        
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
     except:
         topic.publish(Subject='Failed to upload portfolio for Patel Sales Corporation', Message='Please check your PortFolio Deployed Successfully')
     return {
